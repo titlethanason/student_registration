27d2f9b4b09a8179f4002d6787dea52bdb07ff4f
Your Name <you@example.com>
2018-05-26 20:21:49 +0700

    start 26may

--- routes/register.js
+++ routes/register.js
@@ -23,12 +23,15 @@ router.get("/regis",function(req,res){
         res.render("regis");
 });
 router.post("/regis",function(req,res){
+    console.log(jsonByIndex(req.body,1));
+    console.log(jsonValue(req.body,username));
+    console.log(jsonLength(req.body));
     var username = req.body.username;
     var password = req.body.password;
     var role = req.body.role;
     bcrypt.hash(password, saltRounds, function(err, hash) {
         db.query("INSERT INTO users VALUES(?,?,?)",[username,hash,role],function(err,results){
-            if(err) throw err;
+            if(err) res.redirect('back');
             req.login(username,function(err){
                 if(err) throw err;
                 res.redirect("/");
@@ -36,5 +39,15 @@ router.post("/regis",function(req,res){
         })
     })
 });
+
+function jsonValue(obj,key){
+    return obj[key];
+}
+function jsonLength(obj){
+    return Object.keys(obj).length;
+}
+function jsonByIndex(obj, index) {
+    return obj[Object.keys(obj)[index]];
+}
 
 module.exports = router;
